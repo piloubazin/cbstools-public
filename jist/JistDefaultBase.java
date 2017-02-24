1a56ba867ec3e9e90333f46acd38d29d25947c27
Pilou Bazin <piloubazin@gmail.com>
2017-02-24 12:37:53 +0100

    clean compile

---
+++ jist/JistDefaultBase.java
@@ -0,0 +1,17 @@
+package jist.modules;
+
+public class JistDefaultBase implements JistModulesLocation {
+
+	final static String[] default_paths = new String[]{
+		"de.mpg.cbs.jist"
+		"edu.jhu.ece.iacl.jist.plugins",
+		"edu.jhu.ece.iacl.plugins",
+		};
+	
+	@Override
+	public String[] getValidModuleJavaPaths() {
+
+		return default_paths;
+	}
+ 
+}
