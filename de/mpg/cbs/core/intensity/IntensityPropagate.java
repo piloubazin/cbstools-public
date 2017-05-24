143c482e311ccd1ce22d6837ff32fcd1d2cbd95e
Pilou Bazin <piloubazin@gmail.com>
2017-05-24 13:02:40 +0200

    new options for intensity propagation

--- de/mpg/cbs/core/intensity/IntensityPropagate.java
+++ de/mpg/cbs/core/intensity/IntensityPropagate.java
@@ -12,7 +12,8 @@ import de.mpg.cbs.utilities.*;
 public class IntensityPropagate {
 
 	// parameters
-	private		static final String[]	normtypes = {"max","mean","min"};
+	public		static final String[]	normtypes = {"max","mean","min"};
+	public		static final String[]	targettypes = {"zero","mask","lower","higher"};
 	
 	// variables
 	private float[] inputImage = null;
@@ -20,6 +21,8 @@ public class IntensityPropagate {
 	private float[] resultImage;
 	private	String	 normParam = "max";
 	private float distParam = 5;
+	private		String		targetParam = "zero";
+	private float scalingParam = 1.0f;
 	
 	private int nx, ny, nz, nc, nxyz;
 	private float rx, ry, rz;
@@ -27,7 +30,10 @@ public class IntensityPropagate {
 	// set inputs
 	public final void setInputImage(float[] val) { inputImage = val; }
 	public final void setMaskImage(byte[] val) { maskImage = val; }
-	public final void setPropogationDistance(float val) { distParam = val; }
+	public final void setCombinationMethod(String val) { normParam = val; }
+	public final void setPropagationDistance(float val) { distParam = val; }
+	public final void setTargetVoxels(String val) { targetParam = val; }
+	public final void setPropogationScalingFactor(float val) { scalingParam = val; }
 	
 	// set generic inputs	
 	public final void setDimensions(int x, int y, int z) { nx=x; ny=y; nz=z; nc=1; nxyz=nx*ny*nz; }
@@ -55,109 +61,62 @@ public class IntensityPropagate {
 	
 	public void execute() {
 	
-		float[][][] image3 = null;
-		float[][][][] image4 = null;
-	
-		if (nc==1) {
-			image3 = new float[nx][ny][nz];
-			for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
-				image3[x][y][z] = inputImage[x+nx*y+nx*ny*z];
-			}
-		} else {
-			image4 = new float[nx][ny][nz][nc];
-			for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) for (int c=0;c<nc;c++) {
-				image4[x][y][z][c] = inputImage[x+nx*y+nx*ny*z+nx*ny*nz*c];
-			}
-		}
-		// use a mask by default
-		boolean[][][] mask = new boolean[nx][ny][nz];
-		for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
-			mask[x][y][z] = true;
-		}
-		// input mask or mask zero values
-		if (maskImage!=null) {
-			for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
-				mask[x][y][z] = (maskImage[x+nx*y+nx*ny*z]!=0);
-			}
-		} else {
-			for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
-				if (nc==1 && image3[x][y][z]==0) mask[x][y][z] = false;
-				if (nc>1) {
-					mask[x][y][z] = false;
-					for (int c=0;c<nc;c++) if (image4[x][y][z][c]!=0) mask[x][y][z] = true;
-				}
-			}
-		}
-	
+		// different settings
+		byte ZERO = 1, MASK = 2, LOWER = 3, HIGHER = 4;
+		byte target = ZERO;
+		boolean[] mask = null;
+		if (targetParam.equals("mask")) target = MASK;
+		if (targetParam.equals("lower")) target = LOWER;
+		if (targetParam.equals("higher")) target = HIGHER;
+		
 		// main algorithm
 		int nd = Numerics.ceil(distParam/Numerics.min(rx,ry,rz));
-	
+		
 		byte MIN = 1, MAX = 2, MEAN = 3;
 		byte merge = MAX;
 		if (normParam.equals("mean")) merge = MEAN;
 		if (normParam.equals("min")) merge = MIN;
-	
-		float[][][] result3 = null;
-		float[][][][] result4 = null;
-		if (nc==1) result3 = new float[nx][ny][nz];
-		else result4 = new float[nx][ny][nz][nc];
-		byte[][][] count = new byte[nx][ny][nz];
-		for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
-			if (nc==1) result3[x][y][z] = image3[x][y][z];
-			else for (int c=0;c<nc;c++) result4[x][y][z][c] = image4[x][y][z][c];
+
+		// main algorithm
+		resultImage = new float[nxyz*nc];
+		byte[] count = new byte[nxyz];
+		for (int xyzc=0;xyzc<nxyz*nc;xyzc++) {
+			resultImage[xyzc] = inputImage[xyzc];
 		}
 		for (int n=0;n<nd;n++) {
 			System.out.println("step "+(n+1));
-			for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
-				count[x][y][z] = 0;
+			for (int xyz=0;xyz<nxyz;xyz++) {
+				count[xyz] = 0;
 			}
-			for (int x=1;x<nx-1;x++) for (int y=1;y<ny-1;y++) for (int z=1;z<nz-1;z++) if (mask[x][y][z]) {
+			for (int x=1;x<nx-1;x++) for (int y=1;y<ny-1;y++) for (int z=1;z<nz-1;z++) {
+				int xyz = x+nx*y+nx*ny*z;
 		
-				for (int dx=-1;dx<=1;dx++) for (int dy=-1;dy<=1;dy++) for (int dz=-1;dz<=1;dz++) if (!mask[x+dx][y+dy][z+dz]) {
-					//System.out.print(".");
-					if (nc==1) {
+				for (int dx=-1;dx<=1;dx++) for (int dy=-1;dy<=1;dy++) for (int dz=-1;dz<=1;dz++) for (int c=0;c<nc;c++) {
+					int xyzd = xyz + dx+nx*dy+nx*ny*dz;
+					if ( (target==ZERO && inputImage[xyzd + nxyz*c]==0)
+						|| (target==MASK && maskImage[xyzd]==0)
+						|| (target==LOWER && inputImage[xyzd + nxyz*c] < scalingParam*inputImage[xyz + nxyz*c])
+						|| (target==HIGHER && inputImage[xyzd + nxyz*c] > scalingParam*inputImage[xyz + nxyz*c]) ) {
+						//System.out.print(".");
 						if (merge==MIN) {
-							if (count[x+dx][y+dy][z+dz]==0) result3[x+dx][y+dy][z+dz] = result3[x][y][z];
-							else result3[x+dx][y+dy][z+dz] = Numerics.min(result3[x+dx][y+dy][z+dz], result3[x][y][z]);
+							if (count[xyzd]==0) resultImage[xyzd+nxyz*c] = scalingParam*inputImage[xyz+nxyz*c];
+							else resultImage[xyzd+nxyz*c] = Numerics.min(resultImage[xyzd+nxyz*c], scalingParam*inputImage[xyz+nxyz*c]);
 						} else if (merge==MAX) {
-							if (count[x+dx][y+dy][z+dz]==0) result3[x+dx][y+dy][z+dz] = result3[x][y][z];
-							else result3[x+dx][y+dy][z+dz] = Numerics.max(result3[x+dx][y+dy][z+dz], result3[x][y][z]);
+							if (count[xyzd]==0) resultImage[xyzd+nxyz*c] = scalingParam*inputImage[xyz+nxyz*c];
+							else resultImage[xyzd+nxyz*c] = Numerics.max(resultImage[xyzd+nxyz*c], scalingParam*inputImage[xyz+nxyz*c]);
 						} else if (merge==MEAN) {
-							result3[x+dx][y+dy][z+dz] += result3[x][y][z];
+							resultImage[xyzd+nxyz*c] += scalingParam*inputImage[xyz+nxyz*c];
 						}
-					} else {
-						for (int c=0;c<nc;c++) {
-							if (merge==MIN) {
-								if (count[x+dx][y+dy][z+dz]==0) result4[x+dx][y+dy][z+dz][c] = result4[x][y][z][c];
-								else result4[x+dx][y+dy][z+dz][c] = Numerics.min(result4[x+dx][y+dy][z+dz][c], result4[x][y][z][c]);
-							} else if (merge==MAX) {
-								if (count[x+dx][y+dy][z+dz]==0) result4[x+dx][y+dy][z+dz][c] = result4[x][y][z][c];
-								else result4[x+dx][y+dy][z+dz][c] = Numerics.max(result4[x+dx][y+dy][z+dz][c], result4[x][y][z][c]);
-							} else if (merge==MEAN) {
-								result4[x+dx][y+dy][z+dz][c] += result4[x][y][z][c];
-							}
-						}
-					}					
-					count[x+dx][y+dy][z+dz]++;
+						if (c==nc-1) count[xyzd]++;
+					}
 				}
 			}
-			for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) if (count[x][y][z]>0) {
+			for (int xyz=0;xyz<nxyz;xyz++) for (int c=0;c<nc;c++) {
 				if (merge==MEAN) {
-					if (nc==1) result3[x][y][z] /= (float)count[x][y][z];
-					else for (int c=0;c<nc;c++) result4[x][y][z][c] /= (float)count[x][y][z];
+					if (count[xyz]>0) inputImage[xyz+nxyz*c] = resultImage[xyz+nxyz*c]/(1.0f+count[xyz]);
+				} else {
+					inputImage[xyz+nxyz*c] = resultImage[xyz+nxyz*c];
 				}
-				mask[x][y][z] = true;
-			}
-		}
-
-		resultImage = new float[nx*ny*nz*nc];
-		if (nc==1) {
-			for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
-				resultImage[x+nx*y+nx*ny*z] = result3[x][y][z];
-			}
-		} else {
-			for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) for (int c=0;c<nc;c++) {
-				resultImage[x+nx*y+nx*ny*z+nx*ny*nz*c] = result4[x][y][z][c];
 			}
 		}
 	}
