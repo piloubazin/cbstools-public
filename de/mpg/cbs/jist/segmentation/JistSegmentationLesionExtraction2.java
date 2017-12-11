dcc0be0a6069995256dc42eec7c19b83f17d6a0e
Pilou Bazin <bazin@cbs.mpg.de>
2017-12-11 13:18:06 +0100

    new transferred modules

---
+++ de/mpg/cbs/jist/segmentation/JistSegmentationLesionExtraction2.java
@@ -0,0 +1,143 @@
+package de.mpg.cbs.jist.segmentation;
+
+import edu.jhu.ece.iacl.jist.pipeline.AlgorithmRuntimeException;
+import edu.jhu.ece.iacl.jist.pipeline.CalculationMonitor;
+import edu.jhu.ece.iacl.jist.pipeline.ProcessingAlgorithm;
+import edu.jhu.ece.iacl.jist.pipeline.parameter.ParamCollection;
+import edu.jhu.ece.iacl.jist.pipeline.parameter.ParamOption;
+import edu.jhu.ece.iacl.jist.pipeline.parameter.ParamVolume;
+import edu.jhu.ece.iacl.jist.pipeline.parameter.ParamFile;
+import edu.jhu.ece.iacl.jist.pipeline.parameter.ParamInteger;
+import edu.jhu.ece.iacl.jist.pipeline.parameter.ParamFloat;
+import edu.jhu.ece.iacl.jist.pipeline.parameter.ParamBoolean;
+import edu.jhu.ece.iacl.jist.pipeline.parameter.ParamString;
+import edu.jhu.ece.iacl.jist.structures.image.ImageHeader;
+import edu.jhu.ece.iacl.jist.structures.image.ImageData;
+import edu.jhu.ece.iacl.jist.structures.image.ImageDataByte;
+import edu.jhu.ece.iacl.jist.structures.image.ImageDataFloat;
+import edu.jhu.ece.iacl.jist.structures.image.VoxelType;
+import edu.jhu.ece.iacl.jist.io.FileExtensionFilter;
+import edu.jhu.ece.iacl.jist.pipeline.DevelopmentStatus;
+import edu.jhu.ece.iacl.jist.pipeline.ProcessingAlgorithm;
+import edu.jhu.ece.iacl.jist.pipeline.AlgorithmInformation.AlgorithmAuthor;
+import edu.jhu.ece.iacl.jist.pipeline.AlgorithmInformation.Citation;
+import edu.jhu.ece.iacl.jist.pipeline.AlgorithmInformation;
+
+import java.net.URL;
+import java.util.*;
+
+import de.mpg.cbs.utilities.*;
+import de.mpg.cbs.core.segmentation.SegmentationLesionExtraction2;
+
+/*
+ * @author Pierre-Louis Bazin
+ */
+public class JistSegmentationLesionExtraction2 extends ProcessingAlgorithm {
+
+	// jist containers
+	private ParamVolume probaImage;
+	private ParamVolume wmImage;
+	private ParamVolume gmImage;
+	private ParamVolume csfImage;
+	
+	private ParamFloat		wmdistanceParam;
+	private ParamFloat		gmdistanceParam;
+	private ParamFloat		csfdistanceParam;
+	private ParamFloat		lesdistanceParam;
+	private ParamFloat		minprobaThresholdParam;
+	private ParamFloat		maxprobaThresholdParam;
+	private ParamFloat		minSizeParam;
+	
+	private ParamVolume scoreImage;
+	private ParamVolume pvRegionImage;
+	private ParamVolume lesionsizeImage;
+	private ParamVolume lesionprobaImage;
+	private ParamVolume boundarypvImage;
+	private ParamVolume labelImage;
+		
+	private SegmentationLesionExtraction2 algorithm;
+	
+	protected void createInputParameters(ParamCollection inputParams) {
+		
+		inputParams.add(probaImage = new ParamVolume("Probability Image"));
+		inputParams.add(wmImage = new ParamVolume("WM Region Image"));
+		inputParams.add(gmImage = new ParamVolume("GM Background Image"));
+		inputParams.add(csfImage = new ParamVolume("CSF Background Image"));
+		
+		inputParams.add(wmdistanceParam = new ParamFloat("WM boundary partial voluming distance (voxels)",0.0f,100.0f,0.5f));
+		inputParams.add(gmdistanceParam = new ParamFloat("GM boundary partial voluming distance (voxels)",0.0f,100.0f,1.0f));
+		inputParams.add(csfdistanceParam = new ParamFloat("CSF boundary partial voluming distance (voxels)",0.0f,100.0f,2.0f));
+		inputParams.add(lesdistanceParam = new ParamFloat("Lesion clustering distance (voxels)",0.0f,100.0f,2.0f));
+		inputParams.add(minprobaThresholdParam = new ParamFloat("Probability minimum threshold (to include in analysis)",0.0f,1.0f,0.84f));
+		inputParams.add(maxprobaThresholdParam = new ParamFloat("Probability maximum threshold (to label as local maxima)",0.0f,1.0f,0.97f));
+		inputParams.add(minSizeParam = new ParamFloat("Small lesion size (voxels)",0.0f,10000.0f,4.0f));
+
+		algorithm = new SegmentationLesionExtraction2();
+		
+		inputParams.setPackage(algorithm.getPackage());
+		inputParams.setCategory(algorithm.getCategory());
+		inputParams.setLabel(algorithm.getLabel());
+		inputParams.setName(algorithm.getName());
+
+		AlgorithmInformation info = getAlgorithmInformation();
+		info.add(References.getAuthor(algorithm.getAlgorithmAuthors()[0]));
+		info.setAffiliation(algorithm.getAffiliation());
+		info.setDescription(algorithm.getDescription());
+		
+		info.setVersion(algorithm.getVersion());
+		info.setStatus(DevelopmentStatus.RC);
+		info.setEditable(false);
+	}
+
+	@Override
+	protected void createOutputParameters(ParamCollection outputParams) {
+		outputParams.add(scoreImage = new ParamVolume("Lesions score",VoxelType.FLOAT));
+		outputParams.add(pvRegionImage = new ParamVolume("Region partial volume",VoxelType.FLOAT));
+		outputParams.add(lesionsizeImage = new ParamVolume("Lesions size",VoxelType.FLOAT));
+		outputParams.add(lesionprobaImage = new ParamVolume("Lesions probabilities",VoxelType.FLOAT));
+		outputParams.add(boundarypvImage = new ParamVolume("Boundari partial volume",VoxelType.FLOAT));
+		outputParams.add(labelImage = new ParamVolume("Lesion labels",VoxelType.INT));
+		
+		outputParams.setName("lesion images");
+		outputParams.setLabel("lesion images");
+	}
+
+	@Override
+	protected void execute(CalculationMonitor monitor){
+
+		algorithm = new SegmentationLesionExtraction2(); 
+		
+		// i/o variables
+		String probaName = Interface.getName(probaImage);
+		
+		ImageHeader header = Interface.getHeader(probaImage);
+		int[] dims = Interface.getDimensions(probaImage);
+		algorithm.setDimensions(dims);
+		algorithm.setResolutions(Interface.getResolutions(probaImage));
+		
+		algorithm.setProbaImage(Interface.getFloatImage3D(probaImage));
+		algorithm.setWMRegionImage(Interface.getFloatImage3D(wmImage));
+		algorithm.setGMRegionImage(Interface.getFloatImage3D(gmImage));
+		algorithm.setCSFRegionImage(Interface.getFloatImage3D(csfImage));
+		
+		algorithm.setWMPartialVolumingDistance(wmdistanceParam.getValue().floatValue());
+		algorithm.setGMPartialVolumingDistance(gmdistanceParam.getValue().floatValue());
+		algorithm.setCSFPartialVolumingDistance(csfdistanceParam.getValue().floatValue());
+		algorithm.setLesionClusteringDistance(lesdistanceParam.getValue().floatValue());
+		algorithm.setMinProbabilityThreshold(minprobaThresholdParam.getValue().floatValue());
+		algorithm.setMaxProbabilityThreshold(maxprobaThresholdParam.getValue().floatValue());
+		algorithm.setMinimumSize(minSizeParam.getValue().floatValue());
+		
+		algorithm.execute();
+		
+		Interface.setFloatImage3D(algorithm.getLesionScore(), dims, scoreImage, probaName+"_lesion_score", header);
+		Interface.setFloatImage3D(algorithm.getRegionPrior(), dims, pvRegionImage, probaName+"_lesion_prior", header);
+		Interface.setFloatImage3D(algorithm.getLesionSize(), dims, lesionsizeImage, probaName+"_lesion_size", header);
+		Interface.setFloatImage3D(algorithm.getLesionProba(), dims, lesionprobaImage, probaName+"_lesion_proba", header);
+		Interface.setFloatImage3D(algorithm.getBoundaryPartialVolume(), dims, boundarypvImage, probaName+"_lesion_pv", header);
+		Interface.setIntegerImage3D(algorithm.getLesionLabels(), dims, labelImage, probaName+"_lesion_labels", header);
+		
+		return;
+	}
+	
+}
