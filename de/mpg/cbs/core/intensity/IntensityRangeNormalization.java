a94099ca95d0c9b1a91d8d4ad6c474f9ac84a46f
Pilou Bazin <bazin@cbs.mpg.de>
2018-01-22 15:15:54 +0100

    weird behavior fixed

--- de/mpg/cbs/core/intensity/IntensityRangeNormalization.java
+++ de/mpg/cbs/core/intensity/IntensityRangeNormalization.java
@@ -37,7 +37,7 @@ public class IntensityRangeNormalization {
 	public final void setResolutions(float x, float y, float z) { rx=x; ry=y; rz=z; }
 	public final void setResolutions(float[] res) { rx=res[0]; ry=res[1]; rz=res[2]; }
 
-	public final void setNormalization(String val) { normtype = val; }
+	public final void setNormalization(String val) { normParam = val; }
 	public final void setRobustnessRatio(float val) { ratioParam = val; }
 	public final void setNegativeValuesToZero(boolean val) { ignoreNegParam = val; }
 	public final void setIgnoreZeroValues(boolean val) { ignoreZeroParam = val; }
@@ -64,33 +64,38 @@ public class IntensityRangeNormalization {
 		// use a mask by default
 		boolean[] mask = new boolean[nxyz];
 		if (maskImage!=null) {
-			for (int xyz=0;xyz<nx;xyz++) {
+			for (int xyz=0;xyz<nxyz;xyz++) {
 				mask[xyz] = (maskImage[xyz]!=0);
 			}
 		} else {
-			for (int xyz=0;xyz<nx;xyz++) {
+			for (int xyz=0;xyz<nxyz;xyz++) {
 				mask[xyz]  =true;
 			}
 		}
 		// negative to zero
 		if (ignoreNegParam) {
-			for (int xyz=0;xyz<nx;xyz++) {
+			for (int xyz=0;xyz<nxyz;xyz++) {
 				if (inImage[xyz]<0) inImage[xyz] = 0.0f;
 			}
 		}
 		// ignore zero values: masked
 		if (ignoreZeroParam) {
-			for (int xyz=0;xyz<nx;xyz++) {
+			for (int xyz=0;xyz<nxyz;xyz++) {
 				if (inImage[xyz]==0) mask[xyz] = false;
 			}
 		}
-		
+		int ndata=0;
+		for (int xyz=0;xyz<nxyz;xyz++) if (mask[xyz]) ndata++;
+		BasicInfo.displayMessage("data points: "+ndata);    
+		    
 		// main algorithm
 		
 		// 1. estimate the min, max
 		float Imin = 0, Imax = 0;
+		BasicInfo.displayMessage("normalization method: "+normParam+"\n");
+		
 		if (normParam.equals("linear")) {
-			Imin = ImageStatistics.minimum(inImage, mask, nx, ny, nz); 
+		    Imin = ImageStatistics.minimum(inImage, mask, nx, ny, nz); 
 			Imax = ImageStatistics.maximum(inImage, mask, nx, ny, nz);				
 		} else if (normParam.equals("robust")) {
 			Imin = ImageStatistics.robustMinimum(inImage, mask, ratioParam, 4, nx, ny, nz); 
