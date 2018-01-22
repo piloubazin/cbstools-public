8d628678cbc5f03521b05aaf74e32d7dbe9a0d1a
Pilou Bazin <bazin@cbs.mpg.de>
2018-01-22 15:48:49 +0100

    fixing weird bug in indexing

--- de/mpg/cbs/core/intensity/IntensityRangeNormalization.java
+++ de/mpg/cbs/core/intensity/IntensityRangeNormalization.java
@@ -108,7 +108,7 @@ public class IntensityRangeNormalization {
 			
 		// 2. scale the data
 		float[] result = new float[nxyz];
-		for (int xyz=0;xyz<nx;xyz++) {
+		for (int xyz=0;xyz<nxyz;xyz++) {
 			result[xyz] = scalingParam*Numerics.bounded( (inImage[xyz]-Imin)/(Imax-Imin), 0.0f, 1.0f);
 		}
 		resultImage = result;
